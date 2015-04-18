41363304b247b95d19a3eeb9633f187ab45164df
zurs <hampusek96@gmail.com>
2015-04-18 15:19:56 +0000

    Added sendMessage()

--- javascript/chat.js
+++ javascript/chat.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+    connect();
 })
+
+function sendMessage(){
+    var message = document.getElementById("message").value;
+    
+    var json = '{"function":"newMessage", "messageText":"' + message + '"}';
+    var cFunc = function(){};
+    sendToServer(json, cFunc);
+}
