b8c0ed3836a24a1b17f9137f24ee727da4a5f07f
zurs <hampusek96@gmail.com>
2015-04-21 06:16:59 +0000

    Changed alerts

--- javascript/chat.js
+++ javascript/chat.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 function sendMessage(){
     var message = document.getElementById("message").value;
-    
+    alert("Skickar");
     var json = '{"function":"newMessage", "messageText":"' + message + '"}';
     var cFunc = function(){};
     sendToServer(json, cFunc);
