f479fb221179a41cf53d5e451300b2e72bed2ecc
zurs <hampusek96@gmail.com>
2015-05-12 16:30:44 +0000

    Changed the getNewMessages()

--- javascript/server.js
+++ javascript/server.js
@@ -1,4 +1,5 @@
 var lastMessage = "0";
+var oldArray = new Array();
 
 function sendToServer(json, cFunc){ // cFunk står för customFunction som händer när svaret kommer tillbaka
     var nickname = window.localStorage.getItem("nickname");
@@ -17,7 +18,8 @@ function connect(){
         var chatwindow = document.getElementById("conversationwrapper");
         for(var i = 0; i < data.length; i++){
             chatwindow.innerHTML += "<p><span class='messagenick'>" + data[i].senderNick + "</span>: " + data[i].messageText + "</p><br/>";
-            lastMessage = data[i].messageID;
+            oldArray.push(data[i].messageID);
+            //lastMessage = data[i].messageID;
         }
         
         setInterval(getNewMessages, 250);
@@ -26,17 +28,37 @@ function connect(){
 }
 
 function getNewMessages(){ // Körs varje 250 millisekund när man anslutit till servern
-    var json = '{"function":"checkForNewMessages", "lastMessage":"' + lastMessage + '"}';
+    var json = '{"function":"checkForNewMessages"}';
     var cFunc = function(data){
         if(!(data == "false")){
             data = JSON.parse(data);
-            var chatwindow = document.getElementById("conversationwrapper");
+            /* var chatwindow = document.getElementById("conversationwrapper");
             for(var i = 0; i < data.length; i++){
                 chatwindow.innerHTML += "<p><span class='messagenick'>" + data[i].senderNick + "</span>: " + data[i].messageText + "</p><br/>";
                 lastMessage = data[i].messageID;
-            }
+            } */
+            
+            addNewMessages(data, oldArray);
             
         }
     };
     sendToServer(json, cFunc);
+}
+
+//clientSide-kontroll för nya meddelanden
+
+function addNewMessages(newArray, oldArray){
+    var chatwindow = document.getElementById("conversationwrapper");
+    
+    for(var i = 0; i < oldArray.length; i++) {
+        var messageFound = false;
+        for(var j = 0; j < newArray.length; j++) {
+            if(oldArray[i].messageID == newArray[j].messageID) {messageFound = true; }
+            if(!messageFound) { //meddelandet hittades inte i gamla arrayen och måste därför vara ett nytt --> lägg till
+                chatwindow.innerHTML += "<p><span class'messagenick'>" + newArray[j].senderNick + "</span>: " + newArray[j].messageText + "</p><br/>";
+            }
+        }
+    }
+    
+    oldArray = newArray; //sätter nya arrayen till gamla
 }
